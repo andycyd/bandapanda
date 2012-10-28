7cc624ee6521f936385f371682f907ac53e531b1
andycyd <andycyd@hotmail.com>
2012-10-28 17:09:23 +0100

    domingo commit

--- gen/pxc/bandapanda/R.java
+++ gen/pxc/bandapanda/R.java
@@ -11,18 +11,26 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int bambu=0x7f020000;
+        public static final int bpanda=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int editText1=0x7f070000;
-        public static final int menu_settings=0x7f070004;
-        public static final int pager=0x7f070002;
-        public static final int pager_title_strip=0x7f070003;
+        public static final int editText1=0x7f070004;
+        public static final int imageView1=0x7f070003;
+        public static final int laypasswd=0x7f070007;
+        public static final int layusername=0x7f070006;
+        public static final int loginbutton=0x7f070008;
+        public static final int menu_settings=0x7f070009;
+        public static final int pager=0x7f070001;
+        public static final int pager_title_strip=0x7f070002;
+        public static final int searchView1=0x7f070000;
+        public static final int textpasswd=0x7f070005;
     }
     public static final class layout {
         public static final int activity_search=0x7f030000;
+        public static final int wellcome=0x7f030001;
     }
     public static final class menu {
         public static final int activity_search=0x7f060000;
@@ -38,5 +46,6 @@ public final class R {
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
+        public static final int TemaPropi_Negre=0x7f050001;
     }
 }
